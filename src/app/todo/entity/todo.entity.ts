22c1e14095f76c0a300c172631c6fb11b1430645
Ytallo Cesar <ytallobmw@gmail.com>
2023-03-22 18:54:54 -0300

    backand todoapp

---
+++ src/app/todo/entity/todo.entity.ts
@@ -0,0 +1,38 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity({ name: 'todos' })
+export class TodoEntity {
+    @PrimaryGeneratedColumn('uuid')
+    @ApiProperty()
+    id: string;
+
+    @Column()
+    @ApiProperty()
+    task: string;
+
+    @Column({ name: 'is_done', type: 'tinyint', width: 1 })
+    @ApiProperty()
+    isDone: number;
+
+    @CreateDateColumn({ name: 'created_at' })
+    @ApiProperty()
+    createdAt: string;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    @ApiProperty()
+    updatedAt: string;
+
+    @DeleteDateColumn({ name: 'deleted_at' })
+    @ApiProperty()
+    deletedAt: string;
+
+    constructor(todo?: Partial<TodoEntity>) {
+        this.id = todo?.id;
+        this.task = todo?.task;
+        this.isDone = todo?.isDone;
+        this.createdAt = todo?.createdAt;
+        this.updatedAt = todo?.updatedAt;
+        this.deletedAt = todo?.deletedAt;
+    }
+}
